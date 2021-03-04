967aacc57f057f9785b5ff3ecf117e748f4e0ca9
genesisschaerrer <genesisschaerrer.dev@gmail.com>
2021-03-04 12:14:23 -0700

    added footer

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,12 +4,16 @@ import React from 'react';
 
 // import Jumbotron from "../components/jumbotron"
 import JumbortronContainer from "../containers/jumbotron"
+import { FooterContainer} from "../containers/footer"
 
 
 
 function Home() {
     return (
+        <>
          <JumbortronContainer />
+         <FooterContainer />
+        </>
     )
 }
 
